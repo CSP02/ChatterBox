d56e15f45c580f131db91f2614b6b51eaf43f9a4
CSP02 <chandrapilla2002@gmail.com>
2024-05-02 19:45:48 +0530

    trying to change the home page

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const loading = document.getElementById("loading")
 
 window.onload = () => {
     loading.style.display = "none"
+
+    const wrapper = document.getElementById("wrapper")
+    const nav = document.getElementById("navigation")
+    wrapper.addEventListener("scroll", scroll => {
+        wrapper.scrollTop > 0 ? nav.classList.replace("attop", "nattop") : nav.classList.replace("nattop", "attop")
+    })
 }
