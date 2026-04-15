00a63e3ff405cc013e2ba9ad34ffd61d987fe3e1
csp <chandrapilla2002@gmail.com>
2026-04-15 18:21:11 +0530

    changed the link and script paths

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/ValidatePassword", async (req, res) => {
     return res.send({ isValid: cond1 && cond2, charLen8: cond1, mixOfDiff: cond2 }); // return true if both the conditions are true
 })
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Home')));
 
 app.get(`/@me/:channel_id`, async (req, res) => {
     res.sendFile(__dirname + "/@me/");
